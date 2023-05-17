3c29762acdd66911f05d44968101601cd2806f8f
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-05-17 21:49:25 +0200

    Created the searchbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import logo from '/logo.png';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export const Navbar = () => {
   return (
-    <div className="w-full h-24 px-4 mx-auto bg-cyan-900 flex justify-center ">
+    <div className="w-full h-36 sm:h-48 px-4 mx-auto bg-cyan-900 flex flex-col justify-center gap-4">
       <div className="flex items-center justify-center ">
         <img src={logo} className="w-16 sm:w-36" />
         <h1 className="font-montserrat font-semibold text-white text-center sm:text-2xl">
           Biblioteka Dhaskal Todri
         </h1>
+      </div>
+      <div className="w-full h-8 bg-white rounded-lg flex gap-2">
+        <AiOutlineSearch size={'2rem'} className=" ml-2" />
+        <input
+          type="text"
+          placeholder="Kërko për një libër"
+          className="bg-transparent w-full outline-none font-montserrat"
+        />
       </div>
     </div>
   );
